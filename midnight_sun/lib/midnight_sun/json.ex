7180c5e6b6c446352f72c07c1eceedbacd68fef5
xk3 <7908073+chapmanjacobd@users.noreply.github.com>
2020-10-26 15:03:10 +0700

    wip

--- midnight_sun/lib/midnight_sun/json.ex
+++ midnight_sun/lib/midnight_sun/json.ex
@@ -4,37 +4,50 @@ defmodule Cities do
   def load do
     File.read!("../data.json")
     |> Jason.decode!()
-    |> Enum.map(fn x ->
-      sunsetTime = Solar.event(:set, {x["lat"], x["lon"]}, timezone: "UTC")
+  end
 
-      Map.put(x, "sunsetTime", sunsetTime)
+  def sunsets(time \\ Time.utc_now()) do
+    load()
+    |> Enum.map(fn city ->
+      sunsetTime = Solar.event(:set, {city["lat"], city["lon"]}, timezone: "UTC")
+
+      url =
+        "https://www.youtube.com/results?search_query=#{city["n"]}, #{city["c"]}&sp=EgJAAQ%253D%253D"
+
+      city
+      |> Map.put("url", String.replace(url, " ", "%20"))
+      |> Map.put("sunsetTime", sunsetTime)
     end)
     |> Enum.filter(fn x -> elem(x["sunsetTime"], 0) != :error end)
-  end
-
-  def sunsets do
-    load()
     |> Enum.filter(fn x ->
       {:ok, sunsetTime} = x["sunsetTime"]
-      timeLeft = Time.diff(sunsetTime, Time.utc_now())
+      timeLeft = Time.diff(sunsetTime, time)
 
-      0 < timeLeft && timeLeft < 10 * 60
+      0 < timeLeft && timeLeft < 15 * 60
     end)
 
-    # |> Enum.sort(fn a, b ->
-    #   Time.compare(elem(Map.get(a, "sunsetTime"), 1), elem(Map.get(b, "sunsetTime"), 1))
-    # end)
+    # |> Enum.sort(fn a, b -> a["pop"] > b["pop"] end)
   end
 
-  def urls do
-    sunsets()
-    |> Enum.sort(fn a, b -> a["pop"] > b["pop"] end)
-    |> Enum.map(fn city ->
-      "https://www.youtube.com/results?search_query=#{city["n"]}, #{city["c"]}&sp=EgJAAQ%253D%253D"
+  def check do
+    Enum.reduce_while(1..1440, 0, fn currentMinute, acc ->
+      {:ok, time} = Time.new(0, currentMinute, 0)
+
+      chosen_sunset =
+        sunsets(time)
+        |> Enum.reduce_while(%{}, fn city, acc ->
+          url = IO.gets("https / ⏎")
+
+          if url != "\n", do: {:halt, city |> Map.put("live", url)}, else: {:cont, acc}
+        end)
+        |> Enum.at(-1)
+
+      {:ok, sunsetTime} = chosen_sunset["sunsetTime"]
+
+      if chosen_sunset |> length > 0,
+        do: {:cont, sunsetTime.hour * 60 + sunsetTime.minute},
+        else: {:cont, acc + 1}
     end)
-    |> Enum.map(fn x -> String.replace(x, " ", "%20") end)
-    |> Enum.join("\n")
-    |> IO.puts()
   end
 
   #
